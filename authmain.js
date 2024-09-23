5399a2733d1a2088136ec09b85d911c7b2c6e9be
NingggggggYU <doubleone0826@gmail.com>
2024-09-23 16:59:31 +0800

    Update authmain.js

--- authmain.js
+++ authmain.js
@@ -6,18 +6,18 @@ async function configureAuth0() {
     });
 
     try {
-        // 獲取 Token 來檢查是否已登入
-        const token = await auth0Client.getTokenSilently();
-        const isAuthenticated = !!token; // 如果 token 存在則認為已登入
-
+        // 檢查使用者是否已經登入
+        const isAuthenticated = await auth0Client.isAuthenticated();
         console.log("Is Authenticated:", isAuthenticated);
 
         if (!isAuthenticated) {
             console.log("User not authenticated, redirecting to login...");
             await auth0Client.loginWithRedirect({
-                redirect_uri: window.location.origin // 確保重定向回當前網站
+                redirect_uri: window.location.origin
             });
         } else {
+            // 獲取 Token
+            const token = await auth0Client.getTokenSilently();
             const user = await auth0Client.getUser();
             console.log("Logged in as:", user);
 
