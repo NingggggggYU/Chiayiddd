9d358066a169213cbdf64593f5b5ffe18f9849b7
NingggggggYU <doubleone0826@gmail.com>
2024-09-23 17:20:02 +0800

    Update authmain.js

--- authmain.js
+++ authmain.js
@@ -1,10 +1,17 @@
 async function configureAuth0() {
-    // Auth0 設置
     const auth0Client = await createAuth0Client({
         domain: "dev-5m5rsaavaelf0ss3.us.auth0.com",
         client_id: "ldVbBX9hxFRpLfvYbEfQSavq47lZI3lX",
         redirect_uri: "https://ningggggggyu.github.io/Chiayiddd/CALDBS.html"
     });
+
+    // 檢查是否有 Auth0 的回調參數
+    const queryString = window.location.search;
+    if (queryString.includes("code=") && queryString.includes("state=")) {
+        // 處理回調
+        await auth0Client.handleRedirectCallback();
+        window.location.replace(window.location.origin + "/CALDBS.html"); // 重新導向到主要頁面
+    }
 
     // 檢查使用者是否已經登入
     const isAuthenticated = await auth0Client.isAuthenticated();
@@ -20,8 +27,3 @@ async function configureAuth0() {
         console.log("Logged in as:", user);
     }
 }
-
-// 在頁面載入時執行 Auth0 配置和身份驗證邏輯
-window.onload = async () => {
-    await configureAuth0();
-};
