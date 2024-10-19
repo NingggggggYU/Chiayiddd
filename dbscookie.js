eb9285fd7342ecdc82f83bcd3fa31b7cb3ce7c97
NingggggggYU <doubleone0826@gmail.com>
2024-10-19 12:17:50 +0800

    Create dbscookie.js

---
+++ dbscookie.js
@@ -0,0 +1,19 @@
+function getCookie(name) {
+  let value = "; " + document.cookie;
+  let parts = value.split("; " + name + "=");
+  if (parts.length === 2) {
+    return parts.pop().split(";").shift();
+  }
+  return null;
+}
+
+window.onload = function() {
+  let sessionId = getCookie("JSESSIONID");
+
+  if (!sessionId) {
+    console.log("JSESSIONID 不存在，正在跳轉...");
+    window.location.href = "https://example.com"; // 將此處替換為你想跳轉的地址
+  } else {
+    console.log("JSESSIONID 存在:", sessionId);
+  }
+};
