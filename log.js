ab47c7523b0163276c74068bb1c7643bcfc647e3
NingggggggYU <doubleone0826@gmail.com>
2024-10-28 14:48:22 +0800

    Update log.js

--- log.js
+++ log.js
@@ -1,21 +1,22 @@
-<script>
 async function logVisit() {
+  console.log("logVisit 函數開始執行");  // 確認函數被調用
   const website = window.location.href;
   const ipResponse = await fetch('https://api.ipify.org?format=json');
   const ipData = await ipResponse.json();
   const ip = ipData.ip;
   const device = navigator.userAgent;
 
-  const scriptUrl = 'https://script.google.com/macros/s/AKfycbzNxW1O3jEU6mWO--K_r7_jWBCrJA5JmHhRweO5D5NdtlccJMiMNmuY2P6bzp17DP0/exec'; // 使用 Google Apps Script 的 URL
+  const scriptUrl = 'https://script.google.com/macros/s/AKfycbzNxW1O3jEU6mWO--K_r7_jWBCrJA5JmHhRweO5D5NdtlccJMiMNmuY2P6bzp17DP0/exec';
+  console.log("發送資料到 Google Apps Script");
 
-  fetch(scriptUrl, {
+  await fetch(scriptUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ website, ip, device })
-  });
+  }).then(response => console.log("請求已發送", response))
+    .catch(error => console.error("發送請求時出錯", error));
 }
 
 logVisit();
-</script>
