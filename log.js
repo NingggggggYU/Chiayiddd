51786f89145d11d0a00618c8328f8ce51166875c
NingggggggYU <doubleone0826@gmail.com>
2024-10-28 14:23:56 +0800

    Update log.js

--- log.js
+++ log.js
@@ -5,7 +5,7 @@ async function logVisit() {
   const ip = ipData.ip;
   const device = navigator.userAgent;
 
-  const scriptUrl = 'YOUR_SCRIPT_URL'; // 代入您 Google Apps Script 部署的 URL
+  const scriptUrl = 'https://script.google.com/macros/s/AKfycbzNxW1O3jEU6mWO--K_r7_jWBCrJA5JmHhRweO5D5NdtlccJMiMNmuY2P6bzp17DP0/exec'; // 代入您 Google Apps Script 部署的 URL
 
   fetch(scriptUrl, {
     method: 'POST',
