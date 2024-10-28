b42bb92a8e664d14c448813d65f44cdff5014278
NingggggggYU <doubleone0826@gmail.com>
2024-10-28 14:32:02 +0800

    Update log.js

--- log.js
+++ log.js
@@ -1,15 +1,15 @@
+<script>
 async function logVisit() {
   const website = window.location.href;
-  const ipResponse = await fetch('https://api.ipify.org?format=json'); // 使用免費 API 取得 IP 地址
+  const ipResponse = await fetch('https://api.ipify.org?format=json');
   const ipData = await ipResponse.json();
   const ip = ipData.ip;
   const device = navigator.userAgent;
 
-  const scriptUrl = 'https://script.google.com/macros/s/AKfycbzNxW1O3jEU6mWO--K_r7_jWBCrJA5JmHhRweO5D5NdtlccJMiMNmuY2P6bzp17DP0/exec'; // 代入您 Google Apps Script 部署的 URL
+  const scriptUrl = 'https://script.google.com/macros/s/AKfycbzNxW1O3jEU6mWO--K_r7_jWBCrJA5JmHhRweO5D5NdtlccJMiMNmuY2P6bzp17DP0/exec'; // 使用 Google Apps Script 的 URL
 
   fetch(scriptUrl, {
     method: 'POST',
-    mode: 'no-cors',
     headers: {
       'Content-Type': 'application/json'
     },
@@ -18,3 +18,4 @@ async function logVisit() {
 }
 
 logVisit();
+</script>
